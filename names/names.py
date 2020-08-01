8526c2026d682676b5941efa41d229290e147f1e
Jimmique <jimmique88@gmail.com>
2020-07-31 19:21:17 -0700

    Added *.swp to gitignore, added ring_buffer methods, and added BST structure to names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,49 @@
 import time
 
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif (target < self.value) and (self.left is not None):
+            return self.left.contains(target)
+        elif (target > self.value) and (self.right is not None):
+            return self.right.contains(target)
+        else:
+            return False
+
+    def get_max(self):
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+    def for_each(self, fn):
+        fn(self.value)
+
+        if self.left is not None:
+            self.left.for_each(fn)
+        if self.right is not None:
+            self.right.for_each(fn)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -19,8 +63,8 @@ for name_1 in names_1:
             duplicates.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
